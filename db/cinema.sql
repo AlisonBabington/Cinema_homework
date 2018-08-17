b938bc04451f768d81ee88387d8d296aaf23ca16
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-08-17 11:20:08 +0100

    table and file set up created

---
+++ db/cinema.sql
@@ -0,0 +1,24 @@
+DROP TABLE tickets;
+DROP TABLE customers;
+DROP TABLE films;
+
+
+CREATE TABLE customers(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  funds INT4 NOT NULL
+);
+
+CREATE TABLE films(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  price INT4 NOT NULL
+);
+
+CREATE TABLE tickets(
+  id SERIAL8 PRIMARY KEY,
+  customer_id INT8 REFERENCES customers(id)
+  ON DELETE CASCADE,
+  film_id INT8 REFERENCES films(id)
+  ON DELETE CASCADE
+);
